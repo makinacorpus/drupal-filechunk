5cc361414dcc5c4ca4f4d2a8bb62f801e9c81c65
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-02-01 16:25:36 +0100

    now handles multiple values, removed the useless submit button, better theming

--- filechunk.js
+++ filechunk.js
@@ -15,7 +15,7 @@
    *
    * @return void
    */
-  function _upload(url, file, start, step, token, progress, complete) {
+  function _upload(url, file, start, step, token, progress, complete, error) {
     var xhr;
     var stop = Math.min(start + step, file.size);
 
@@ -29,6 +29,7 @@
     }
     xhr.overrideMimeType("application/octet-stream");
 
+    xhr.onerror = error;
     xhr.onload = function () {
       var response;
       try {
@@ -36,6 +37,7 @@
       } catch (e) {
         // @todo Error handling
         response = {};
+        error();
       }
       if (file.size <= stop || response.finished) {
         complete(response);
@@ -43,9 +45,9 @@
       } else {
         progress(Math.round((response.offset / file.size) * 100));
         if (response.resume && response.offset) {
-          _upload(url, file, response.offset, step, token, progress, complete);
+          _upload(url, file, response.offset, step, token, progress, complete, error);
         } else {
-          _upload(url, file, stop, step, token, progress, complete);
+          _upload(url, file, stop, step, token, progress, complete, error);
         }
       }
     };
@@ -69,18 +71,51 @@
   }
 
   /**
-   * Create a clone of the given element and replace the previous with it.
+   * Simple warning message when the window closes.
    *
-   * @param DOMNode element
-   *
-   * @return DOMNode
+  function _warn() {
+    return Drupal.t("There is a file upload in progress, are you sure you want to close this window ?");
+  }
    */
-  function _emptyValue(element) {
-    var previous = $(element);
-    var replacement = previous.val('').clone(true);
-    previous.replaceWith(replacement);
-    previous.remove();
-    return replacement.get(0);
+
+  /**
+   * Disable element.
+   *
+   * @param DOMNode|selector element
+   */
+  function _disable(element) {
+    $(element).attr('disabled', 'disabled').addClass('disabled');
+  }
+
+  /**
+   * Enable element.
+   *
+   * @param DOMNode|selector element
+   */
+  function _enable(element) {
+    $(element).removeAttr('disabled').removeClass('disabled');
+  }
+
+  /**
+   * Show element.
+   *
+   * @param DOMNode|selector element
+   */
+  function _show(element) {
+    $(element).each(function () {
+      this.style.display = null;
+    });
+  }
+
+  /**
+   * Hide element.
+   *
+   * @param DOMNode|selector element
+   */
+  function _hide(element) {
+    $(element).each(function () {
+      this.style.display = "none";
+    });
   }
 
   /**
@@ -108,54 +143,27 @@
         (function (id) {
 
           var
-            upload  = $(context).find("#" + id),
-            form    = upload.closest('form'),
-            inputs  = form.find('input:enabled'),
-            parent  = upload.closest('.filechunk-widget'),
-            preview = parent.find('.filechunk-thumbnail'),
-            submit  = parent.find(".filechunk-upload"),
-            remove  = parent.find(".filechunk-remove"),
-            drop    = parent.find('.filechunk-drop'),
-            valFid  = parent.find("[rel=fid]"),
-            valHash = parent.find("[rel=hash]"),
-            bar     = parent.find('.file-progress'),
+            upload      = $(context).find("#" + id),
+            formInputs  = upload.closest('form').find('input:enabled:not(.filechunk-remove)'),
+            parent      = upload.closest('.filechunk-widget'),
+            items       = parent.find('.filechunk-thumbnail'),
+            value       = settings.defaults,
+            valueInput  = parent.find("[rel=fid]"),
+            bar         = parent.find('.file-progress'),
             files, file, i
           ;
 
+          // Javascript is active, therefore we need to remove graceful
+          // downgrade stuff before proceeding.
+          parent.find('.filechunk-drop').remove();
+          parent.find('[rel=downgrade]').val('');
+          upload.css({opacity: 0, position: "absolute", top: 0, left: 0, bottom: 0, right: 0});
           upload = upload.get(0);
-          drop.hide();
 
-          var _updateProgress = function (percent) {
-            var value = parseInt(percent, 10) + '%';
-            bar.find('.progress-bar').css('width', value).html(value);
-          };
-
-          var _refresh = function () {
-            bar.hide();
-            if (valFid.val()) {
-              preview.show();
-              submit.hide();
-              submit.attr('disabled', 'disabled').addClass('disabled');
-              remove.removeAttr('disabled').removeClass('disabled');
-              remove.show();
-              upload = _emptyValue(upload);
-              $(upload).hide();
-              drop.removeAttr('checked');
-            } else{
-              preview.hide();
-              remove.hide();
-              remove.attr('disabled', 'disabled').addClass('disabled');
-              submit.removeAttr('disabled').removeClass('disabled');
-              submit.show();
-              upload = _emptyValue(upload);
-              $(upload).show();
-              drop.attr('checked', 'checked');
-            }
-          };
-
-          _refresh();
-
-          submit.on("click", function (event) {
+          /**
+           * For whatever that happens, this will run the file upload.
+           */
+          var _onAnythingDoUpload = function (event) {
             event.stopPropagation();
             event.preventDefault();
 
@@ -164,53 +172,120 @@
               return false;
             }
 
-            bar.show();
-
-            if (form) {
-              // Should disable all submit and buttons elements.
-              inputs.attr('disabled', 'disabled').addClass('disabled');
-            }
+            _show(bar);
+            _disable(formInputs);
 
             // Now the hard part.
             for (i = 0, file; file = files[i]; ++i) {
               _upload(settings.url.upload, file, 0, settings.chunksize, settings.token, _updateProgress, function (response) {
                 if (response.finished) {
-                  // If we have a file, populate the form as-is.
-                  if (response.fid) {
-                    valFid.val(response.fid);
-                  }
-                  if (response.hash) {
-                    valHash.val(response.hash);
-                  }
-                  if (response.preview) {
-                    preview.html(response.preview);
-                  } else {
-                    if (response.filename) {
-                      preview.html("<span class=\"text-muted\">" + response.filename + "</span>");
-                    } else {
-                      preview.html("<span class=\"text-muted\">" + Drupal.t("Upload complete") + "</span>");
-                    }
-                  }
+                  _addItem(response.fid, response.hash, response.preview);
                 }
-                // Re-enabled inputs when finished.
-                inputs.removeAttr('disabled').removeClass('disabled');
+              }, function () {
                 _refresh();
               });
             }
+          };
 
-            return false;
-          });
+          /**
+           * Create a clone of the given element and replace the previous with it.
+           *
+           * @param DOMNode element
+           *
+           * @return DOMNode
+           */
+          function _recreateUploadInput(element) {
+            var previous = $(element);
+            var replacement = previous.val('').clone(true);
+            previous.replaceWith(replacement);
+            previous.remove();
+            $(replacement).on("change", _onAnythingDoUpload);
+            return replacement.get(0);
+          }
 
-          remove.on("click", function (event) {
+          /**
+           * Update progress bar callback.
+           */
+          var _updateProgress = function (percent) {
+            var value = parseInt(percent, 10) + '%';
+            bar.find('.progress-bar').css('width', value).html(value);
+          };
+
+          /**
+           * Restore value into the right form input.
+           */
+          var _updateValue = function () {
+            valueInput.val(JSON.stringify(value));
+            _refresh();
+          };
+
+          /**
+           * Refresh UI (enable, disable buttons and submits).
+           */
+          var _refresh = function () {
+            var hasValue = false;
+            // Attempt deactivation of submit widget when in not multiple mode
+            // to ensure that only one value may be uploaded.
+            if (!settings.multiple) {
+              $.each(value, function (index) {
+                // "null" exists when delete is being used... WTF JS.
+                if (index && "null" !== index) {
+                  hasValue = true;
+                }
+              });
+              if (hasValue) {
+                _disable(upload);
+              } else {
+                _enable(upload);
+              }
+            }
+            upload = _recreateUploadInput(upload);
+            _hide(bar);
+            _enable(formInputs);
+          };
+
+          /**
+           * Add new item to item list.
+           */
+          var _addItem = function (fid, hash, preview, item) {
+            // When adding items in javascript, we don't care about the 'drop'
+            // checkbox, since it will be fully handled by the javascript code.
+            var remove = $(settings.template.remove);
+            if (!item) {
+              item = $('<li data-fid="' + fid + '"></li>');
+            }
+            $(items).append(item.append(preview).append(remove.on('click', _removeOnClick)));
+            value[fid] = hash;
+            _updateValue();
+          };
+
+          /**
+           * Remove item from item list.
+           */
+          var _removeItem = function (fid) {
+            $(items).find('[data-fid=' + fid + ']').remove();
+            delete value[fid];
+            _updateValue();
+          };
+
+          /**
+           * Remove item from server callback.
+           */
+          var _removeOnClick = function (event) {
             event.stopPropagation();
             event.preventDefault();
-            var fid = valFid.val();
+            var item = $(this).closest('li'), fid = item.attr('data-fid');
             _remove(settings.url.remove, fid, settings.token);
-            valFid.val('');
-            _refresh();
-            return false;
-          });
+            _removeItem(fid);
+          };
 
+          // Adds the missing remove buttons from the start.
+          items.find('li').each(function () {
+            _addItem(null, null, null, $(this));
+          })
+          _refresh();
+
+          $(upload).on("change", _onAnythingDoUpload);
         }(id));
       }
     }
